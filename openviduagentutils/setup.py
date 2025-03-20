82be5a95e766fe18cbf507c62c469598948ca289
pabloFuente <pablofuenteperez@gmail.com>
2025-03-20 14:00:20 +0100

    Update setup.py of common package

--- openviduagentutils/setup.py
+++ openviduagentutils/setup.py
@@ -1,13 +1,12 @@
-import setuptools
-import setuptools.command.build_py
+from setuptools import setup, find_packages
 
-setuptools.setup(
+setup(
     name="openviduagentutils",
     version="0.0.1",
     description="Helper utilities for OpenVidu AI Agents",
     url="https://github.com/openvidu/openvidu-agents",
     license="Apache-2.0",
-    packages=["openviduagentutils"],
+    packages=find_packages(),
     python_requires=">=3.9.0",
     install_requires=["redis>=5.2.1", "python-dotenv>=1.0.1"],
     author="Pablo Fuente",

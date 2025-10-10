bb36af217b3e061c00536c904a8490b8bbc96020
pabloFuente <pablofuenteperez@gmail.com>
2025-10-10 11:04:34 +0200

    Fix local library imports

--- openviduagentutils/openviduagentutils/__init__.py
+++ openviduagentutils/openviduagentutils/__init__.py
@@ -1,4 +1,5 @@
 from .not_provided import NOT_PROVIDED  # re-export for convenience
+from .config_manager import ConfigManager  # re-export for convenience
 
 # Public API surface
-__all__ = ["NOT_PROVIDED"]
+__all__ = ["NOT_PROVIDED", "ConfigManager"]
